81b75caae47e219dafef0aa4df7a6cce09eb3c62
tf1158 <51709169+tf1158@users.noreply.github.com>
2019-06-25 16:15:53 +0100

    Update custom-scripts.js

--- app/custom-scripts.js
+++ app/custom-scripts.js
@@ -18,28 +18,9 @@ define(["dojo/topic"], function(topic) {
 	topic.subscribe("story-loaded-map", function(result){
 		if ( result.id == WEBMAP_ID && ! clickHandlerIsSetup ) {
 			var map = app.maps[result.id].response.map,
-			layer = map.getLayer(LAYER_ID);
+				layer = map.getLayer(LAYER_ID);
 
 			if ( layer ) {
-        // On mouseover, change cursor to pointer; show tooltip
-				layer.on("mouse-over", function(e){
-					map.setMapCursor("pointer");
-					map.infoWindow.setTitle("<div style='font-weight:bold;font-size:14px'>"+e.graphic.attributes.Name+"</div>"
-						);
-					map.infoWindow.setContent(
-						"<div>"+e.graphic.attributes.Location+"</div>"
-						);
-					map.infoWindow.show(e.graphic.geometry);
-					map.infoWindow.resize(300,200);
-				});
-        
-        //On mouseout, revert to default cursor and hide tooltip
-				layer.on("mouse-out", function(e){
-					map.setMapCursor("default");
-					map.infoWindow.hide();
-				});
-        
-        //On click, scroll to corresponding MJ section
 				layer.on("click", function(e){
 					var index = e.graphic.attributes["SectionIndex"];
 					topic.publish("story-navigate-section", index);
